b4a1f14d5a9728be60fe959dea383deb3d0aa0de
Francisco Llanquipichun <francisco.llanquipichun@gmail.com>
2021-07-02 23:19:42 -0400

    fix deploy congifg
    Signed-off-by: Francisco Llanquipichun <francisco.llanquipichun@gmail.com>

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,7 @@ set -e
 # build
 echo '🛠  Build project dist'
 
-yarn run build
+yarn build
 
 # navigate into the build output directory
 cd dist
@@ -16,14 +16,14 @@ echo 'francisco.llanquipichun.cl' > CNAME
 # add repository readme
 echo 'Deployed code for website [francisco.llanquipichun.cl](https://francisco.llanquipichun.cl)' > README.md
 
-echo '🚀  Deploying in abogados-zona-sur/static-website repository'
+echo '🚀  Deploying in franciscollanquipichun/franciscollanquipichun.github.io.git'
 
 git init
 git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
-# git push -f git@github.com:franciscollanquipichun/franciscollanquipichun.github.io.git main
+git push -f git@github.com:franciscollanquipichun/franciscollanquipichun.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:franciscollanquipichun/gh-pages-test.git main:gh-pages
--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,3 @@
 module.exports = {
-    publicPath: process.env.NODE_ENV === 'production'
-      ? '/fco-llanquipichun-site/'
-      : '/'
+    publicPath: './'
   }
